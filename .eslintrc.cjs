08ba0c93b482337dc51e6a6aaada68e8d73f0bcc
doinel1a <doinel1atanasiu@gmail.com>
2023-07-13 14:28:53 +0200

    feat: eslint configured

---
+++ .eslintrc.cjs
@@ -0,0 +1,38 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+	extends: ['plugin:jsx-a11y/recommended'],
+	overrides: [
+		{
+			files: ['*.astro'],
+			plugins: ['astro'],
+			env: {
+				node: true,
+				'astro/astro': true,
+				es2020: true,
+			},
+			parser: 'astro-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+				extraFileExtensions: ['.astro'],
+				sourceType: 'module',
+			},
+			rules: {
+				'astro/no-conflict-set-directives': 'error',
+				'astro/no-unused-define-vars-in-style': 'error',
+			},
+		},
+		{
+			files: ['**/*.astro/*.js', '*.astro/*.js'],
+			env: {
+				browser: true,
+				es2020: true,
+			},
+			parserOptions: {
+				sourceType: 'module',
+			},
+			rules: {
+				'prettier/prettier': 'off',
+			},
+		},
+	],
+};

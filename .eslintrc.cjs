9314e83771dcb0d2c44c9ec3babad9d99040dd53
doinel1a <doinel1atanasiu@gmail.com>
2023-07-13 15:26:16 +0200

    feat: eslint import sorting plugins configured

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,6 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
+  plugins: ['simple-import-sort', 'import'],
   extends: ['plugin:jsx-a11y/recommended', 'plugin:prettier/recommended'],
   overrides: [
     {
@@ -34,5 +35,42 @@ module.exports = {
         'prettier/prettier': 'off'
       }
     }
-  ]
+  ],
+  rules: {
+    // base
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    // end
+
+    // prettier
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        tabWidth: 2,
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+        jsxSingleQuote: true,
+        trailingComma: 'none',
+        bracketSpacing: true,
+        bracketSameLine: false,
+        arrowParens: 'always',
+        endOfLine: 'crlf'
+      }
+    ],
+    // end prettier
+
+    // simple-import-sort
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error'
+    // end simple-import-sort
+  }
 };

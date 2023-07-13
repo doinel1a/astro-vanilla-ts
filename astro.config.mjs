1bfd98343a9cec01bd5336ca81b77f324f3a813a
doinel1a <doinel1atanasiu@gmail.com>
2023-07-13 14:58:32 +0200

    feat: prettier configured

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-	integrations: [tailwind()],
+  integrations: [tailwind()]
 });

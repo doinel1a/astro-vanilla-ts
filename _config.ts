8eb8cf086b0364694ce3e10e9c8eaa6f9e4bd243
doinel1a <doinel1atanasiu@gmail.com>
2023-07-22 19:07:06 +0200

    refactor: template base content

---
+++ _config.ts
@@ -0,0 +1,4 @@
+const BUSINESS_LINK = 'https://www.business-link.d1a.app';
+const GITHUB_REPOSITORY_LINK = 'https://github.com/doinel1a/astro-vanilla-ts';
+
+export { BUSINESS_LINK, GITHUB_REPOSITORY_LINK };

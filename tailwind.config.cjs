4f8cb929e927d97a5ca980b302d18901ae13021c
doinel1a <doinel1atanasiu@gmail.com>
2023-07-22 19:07:06 +0200

    refactor: template base content

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
-  darkMode: 'class'
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        tertiary: 'var(--tertiary)',
+        'color-light': 'var(--color-light)',
+        'color-dark': 'var(--color-dark)',
+        'accent-primary': 'var(--accent-primary)',
+        'accent-primary-state': 'var(--accent-primary-state)'
+      }
+    }
+  }
 };

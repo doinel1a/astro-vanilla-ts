527f23c322dc7264551c142bc6d5d8eebfb50db2
doinel1a <doinel1atanasiu@gmail.com>
2023-07-23 17:21:45 +0200

    feat: meta data

--- _config.ts
+++ _config.ts
@@ -1,4 +1,5 @@
-const BUSINESS_LINK = 'https://www.business-link.d1a.app';
+const DOMAIN = 'astro-vanilla-ts.d1a.app';
+const BUSINESS_LINK = 'https://business-link.d1a.app';
 const GITHUB_REPOSITORY_LINK = 'https://github.com/doinel1a/astro-vanilla-ts';
 
-export { BUSINESS_LINK, GITHUB_REPOSITORY_LINK };
+export { DOMAIN, BUSINESS_LINK, GITHUB_REPOSITORY_LINK };

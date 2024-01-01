6c4ebf042057d791d5106e88cb5dfde7bf3ae5cd
doinel1a <doinel1atanasiu@gmail.com>
2024-01-01 21:33:47 +0100

    refactor: minor updates

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: [require('tailwindcss'), require('autoprefixer'), require('cssnano')]
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    cssnano: {}
+  }
 };

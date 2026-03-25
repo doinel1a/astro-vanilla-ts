bcb41ad130c67140c88a954f9aafa6db40f03ae4
doinel1a <doinel1atanasiu@gmail.com>
2026-03-25 22:28:42 +0100

    chore: install & configure astro compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,17 @@ import path from 'node:path';
 
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@tailwindcss/vite';
+import compress from 'astro-compress';
 import { defineConfig } from 'astro/config';
 
 import { DOMAIN } from './_config';
 
 export default defineConfig({
   site: `https://${DOMAIN}`,
-  integrations: [sitemap()],
+  integrations: [
+    sitemap(),
+    compress() // Must be last
+  ],
   vite: {
     plugins: [tailwind()],
     resolve: {

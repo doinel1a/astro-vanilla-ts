8b986bf7c79c4457e319ad44c0fe39efa9c375a3
doinel1a <doinel1atanasiu@gmail.com>
2026-03-25 22:27:05 +0100

    chore: install & configure astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import path from 'node:path';
 
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import { DOMAIN } from './_config';
+
 export default defineConfig({
-  integrations: [],
+  site: `https://${DOMAIN}`,
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwind()],
     resolve: {

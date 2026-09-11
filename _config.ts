fb4c50f29093c620267253b2df06f493e6d66d6d
doinel1a <doinel1atanasiu@gmail.com>
2026-09-11 19:45:14 +0200

    chore: update business link

--- _config.ts
+++ _config.ts
@@ -1,5 +1,5 @@
 const DOMAIN = 'astro-vanilla-ts.d1a.app';
-const BUSINESS_LINK = 'https://business-link.d1a.app';
+const BUSINESS_LINK = 'https://links.atanasiu.me';
 const GITHUB_REPOSITORY_LINK = 'https://github.com/doinel1a/astro-vanilla-ts';
 
 export { DOMAIN, BUSINESS_LINK, GITHUB_REPOSITORY_LINK };
